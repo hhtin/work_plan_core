d8c584954d63c4ce4b338775fb74d03c8010040b
tinhh <tinhhdev@gmail.com>
2024-05-09 19:11:13 +0700

    first commit

---
+++ src/main/java/vn/tinhh/workplan/WorkPlanCoreApplication.java
@@ -0,0 +1,13 @@
+package vn.tinhh.workplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPlanCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkPlanCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/tinhh/workplan/WorkPlanCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tinhh.workplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPlanCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
