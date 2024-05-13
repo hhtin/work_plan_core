30b6b8ddc15e6879954e190a0ed370f136e63d71
tinhh <tinhhdev@gmail.com>
2024-05-13 22:06:14 +0700

    ++update

--- src/main/java/vn/tinhh/workplan/WorkPlanCoreApplication.java
+++ src/main/java/vn/tinhh/workplan/WorkPlanCoreApplication.java
@@ -2,12 +2,19 @@ package vn.tinhh.workplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WorkPlanCoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorkPlanCoreApplication.class, args);
     }
 
+    @GetMapping("/simple-request")
+    public String simpleRequest() {
+        return "oke";
+    }
 }
